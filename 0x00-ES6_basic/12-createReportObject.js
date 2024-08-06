980dc8be0e615dfc4d00ef01db32993611bf0be8
Anas Asimi <anas2020cr7@gmail.com>
2024-08-07 00:23:24 +0100

    fix 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,14 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: { ...employeesList } };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employees) {
+      let count = 0;
+      for (const key in employees) {
+        if (Object.prototype.hasOwnProperty.call(employees, key)) {
+          count += 1;
+        }
+      }
+      return count;
+    },
+  };
 }
