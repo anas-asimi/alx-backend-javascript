5c6faad2a7d690bab51939971e2d50d1eccee02c
Anas Asimi <anas2020cr7@gmail.com>
2024-08-07 00:08:49 +0100

    fix 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    idx = appendString + idx;
+  const arr = [];
+  for (const idx of array) {
+    arr.push(appendString + idx);
   }
 
-  return array;
+  return arr;
 }
