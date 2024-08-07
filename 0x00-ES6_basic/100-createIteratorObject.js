febcb30a6a301447de8a029fa0cc9e77a10f84a4
Anas Asimi <anas2020cr7@gmail.com>
2024-08-07 22:06:48 +0100

    fix lint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
-  let arr = []
+  const arr = [];
   for (const key in report.allEmployees) {
     if (Object.prototype.hasOwnProperty.call(report.allEmployees, key)) {
-      let employees = report.allEmployees[key]
+      const employees = report.allEmployees[key];
       for (const employe of employees) {
-        arr.push(employe)
+        arr.push(employe);
       }
     }
   }
-  return arr
+  return arr;
 }
