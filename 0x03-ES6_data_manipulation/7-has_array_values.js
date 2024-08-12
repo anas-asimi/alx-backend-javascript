f7c0033a836f7b26c9e7c02fa940b51f8389b3bf
Anas Asimi <anas2020cr7@gmail.com>
2024-08-12 20:33:49 +0100

    update

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,4 @@
 export default function setFromArray(mySet, myArray) {
-  for (const ele of myArray) {
-    if (!mySet.has(ele)) return false;
-  }
+  for (const ele of myArray) if (!mySet.has(ele)) return false;
   return true;
 }
