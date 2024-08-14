77bbd492c06ae74b9276ac59888edb27984060d6
Anas Asimi <anas2020cr7@gmail.com>
2024-08-14 23:03:29 +0100

    fix task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,8 +5,11 @@ export default function updateStudentGradeByCity(
 ) {
   return listStudents
     .filter((ele) => ele.location === city)
-    .map((ele) => ({
-      ...ele,
-      grade: newGrades.find((x) => x.studentId === ele.id) || 'N/A',
-    }));
+    .map((ele) => {
+      const newGrade = newGrades.find((x) => x.studentId === ele.id);
+      return {
+        ...ele,
+        grade: newGrade !== undefined ? newGrade.grade : 'N/A',
+      };
+    });
 }
