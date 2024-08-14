a0443fbdc3b7130ed77fcb70bf6f102258eeb262
Anas Asimi <anas2020cr7@gmail.com>
2024-08-14 23:07:40 +0100

    fix task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const dv = new DataView(new Int8Array(length));
-  dv.setInt8(position, value);
+  const dv = new DataView(new ArrayBuffer(length));
+  try {
+    dv.setInt8(position, value);
+  } catch (error) {
+    throw new Error('Position outside range');
+  }
   return dv;
 }
