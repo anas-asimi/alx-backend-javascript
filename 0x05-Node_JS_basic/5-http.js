53093101fe8c909cf06713c136e46d8be8e4160d
Anas Asimi <anas2020cr7@gmail.com>
2024-09-30 19:03:12 +0100

    task 5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const path = process.argv[2]
+
+const app = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.end('Hello Holberton School!');
+    }
+    if (req.url == '/students') {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                res.end('Cannot load the database\n')
+            }
+            else {
+                const rows = data
+                    .trim()
+                    .split('\n')
+                    .slice(1)
+                    .map(e => e.split(','))
+                const fields = new Set(rows.map(e => e[3]))
+                res.write('This is the list of our students\n');
+                res.write(`Number of students: ${rows.length}\n`);
+                for (const field of fields) {
+                    students = rows.filter(e => e[3] === field)
+                    res.write(`Number of students in ${field}: ${students.length}. List: ${students.map(e => e[0]).join(', ')}\n`);
+                }
+                res.end()
+            }
+        });
+    }
+});
+
+app.listen(1245)
+
+module.exports = app
