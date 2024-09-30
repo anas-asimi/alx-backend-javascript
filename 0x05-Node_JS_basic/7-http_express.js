aa58724b5f6ca4fddddc823d5be675049cfa8c0e
Anas Asimi <anas2020cr7@gmail.com>
2024-09-30 19:17:07 +0100

    task 7. Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+
+
+const path = process.argv[2]
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            res.end('Cannot load the database\n')
+        }
+        else {
+            const rows = data
+                .trim()
+                .split('\n')
+                .slice(1)
+                .map(e => e.split(','))
+            const fields = new Set(rows.map(e => e[3]))
+            res.write('This is the list of our students\n');
+            res.write(`Number of students: ${rows.length}\n`);
+            for (const field of fields) {
+                students = rows.filter(e => e[3] === field)
+                res.write(`Number of students in ${field}: ${students.length}. List: ${students.map(e => e[0]).join(', ')}\n`);
+            }
+            res.end()
+        }
+    });
+});
+
+app.listen(1245);
+
+module.exports = app
