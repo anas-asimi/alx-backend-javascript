ab7b7f6b0dc1ca2ea9b48cec2695874e296c2420
Anas Asimi <anas2020cr7@gmail.com>
2024-10-02 20:58:52 +0100

    fix 2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    if (fs.existsSync(path)) {
-        const csvData = fs.readFileSync(path, 'utf8');
-        const rows = csvData
-            .trim()
-            .split('\n')
-            .slice(1)
-            .map(e => e.split(','))
+  if (fs.existsSync(path)) {
+    const csvData = fs.readFileSync(path, 'utf8');
+    const rows = csvData
+      .trim()
+      .split('\n')
+      .slice(1)
+      .map((e) => e.split(','));
 
-        const fields = new Set(rows.map(e => e[3]))
-        console.log(`Number of students: ${rows.length}`);
-        for (const field of fields) {
-            students = rows.filter(e => e[3] === field)
-            console.log(`Number of students in ${field}: ${students.length}. List: ${students.map(e => e[0]).join(', ')}`);
-        }
-    } else console.log('Cannot load the database');
+    const fields = new Set(rows.map((e) => e[3]));
+    console.log(`Number of students: ${rows.length}`);
+    for (const field of fields) {
+      const students = rows.filter((e) => e[3] === field);
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.map((e) => e[0]).join(', ')}`);
+    }
+  } else {
+    throw new Error('Cannot load the database');
+  }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
